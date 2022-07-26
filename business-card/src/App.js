d8358e982802c2df818d24e9bda9ef53bcb26f89
xlan11 <alankelly@gmail.com>
2022-07-26 21:26:04 +0100

    commit

---
+++ business-card/src/App.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+import About from "./components/About"
+import Footer from "./components/Footer"
+import Info from "./components/Info"
+import Interests from "./components/Interests"
+import "./style.css"
+
+export default function App() {
+    return (
+        <div className="container"> 
+            <Info />
+            <About />
+            <Interests />
+            <Footer />
+        </div>
+    )
+}
---
+++ business-card/src/components/Info.js
@@ -0,0 +1,16 @@
+import React from "react"
+export default function Info() {
+    return(
+        <div className="container">
+            <div className="info--img">
+            </div>
+            <h1 className="info--h1">Alan Kelly</h1>
+            <h4>Frontend Developer</h4>
+            <a href="website.com">website.com</a>
+            <div className="btn--container">
+            <button className="btn--email">Email</button>
+            <button className="btn--linkedin">LinkedIn</button>
+            </div>
+        </div>
+    )
+}
